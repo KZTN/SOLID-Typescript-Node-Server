81ab524abdeec8e9f421042aad77de9910ebf85e
KZTN <kakaiocesar2@gmail.com>
2020-07-30 19:55:15 -0300

    :art: Structured relation Appointment to User
    all changes are functional

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,23 @@
+import { getRepository } from 'typeorm';
+import User from '../models/User';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const usersRepository = getRepository(User);
+    const checkUserExists = await usersRepository.findOne({ where: { email } });
+    if (checkUserExists) {
+      throw new Error('email already registered');
+    }
+    const user = usersRepository.create({ name, email, password });
+    await usersRepository.save(user);
+    return user;
+  }
+}
+
+export default CreateUserService;
